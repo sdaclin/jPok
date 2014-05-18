778d084c5f485a66f82ee6cdb7bab455ad9619de
sdaclin@gmail.com <git1Pass>
2014-05-18 16:07:44 +0200

    - Adjustement in blind schema and starting stack

--- business/src/main/java/io/github/sdaclin/jpok/Main.java
+++ business/src/main/java/io/github/sdaclin/jpok/Main.java
@@ -19,15 +19,17 @@ public class Main {
     // Create a schema to handle blind leveling up during the game
     BlindSchema blindSchema = new BlindSchema.Builder()
         .addRank(new BlindRank(5, 20, 10, 0))
-        .addRank(new BlindRank(5, 40, 20, 0))
-        .addRank(new BlindRank(5, 60, 30, 0))
+        .addRank(new BlindRank(5, 30, 15, 0))
+        .addRank(new BlindRank(5, 50, 25, 0))
         .addRank(new BlindRank(5, 100, 50, 0))
-        .addRank(new BlindRank(5, 200, 100, 0))
+        .addRank(new BlindRank(5, 150, 75, 0))
+        .addRank(new BlindRank(5, 200, 100, 20))
         .addRank(new BlindRank(5, 400, 200, 20))
+        .addRank(new BlindRank(5, 600, 300, 20))
         .build();
 
     // Create a party for 6 player according to the blind schema and with a 2.000 starting stack
-    Party party = new Party(6, blindSchema, 2000);
+    Party party = new Party(6, blindSchema, 1500);
 
     // Create a new manager to handle this party
     partyManager = new PartyManager(party);
